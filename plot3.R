82adb554fe54e8f056f3b1c079331ba039c74bd5
Aparna <aparna4agarwal@gmail.com>
2019-01-02 15:31:18 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-dataFile <- "./data/household_power_consumption.txt"
+dataFile <- "./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
@@ -15,3 +15,25 @@ lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
+
+# Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
+# calling the basic plot functions
+plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+# annotating graph
+title(main="Energy sub-metering")
